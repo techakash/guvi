97084729b470131386813e500b929f235b612ce4
techakash <35379867+techakash@users.noreply.github.com>
2018-01-18 23:41:20 +0530

    Update vowel.c

--- vowel.c
+++ vowel.c
@@ -3,7 +3,7 @@ void main()
 {
 char c;
 scanf("%c",&c);
-if(c==a||c==e||c==i||c==o||c==u||c==A||c==E||c==I||c==O||c==U)
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
 {
 printf("Vowel");
 }
